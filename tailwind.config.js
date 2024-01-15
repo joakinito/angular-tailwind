18eb6642d4a36a9b26cf18275c42f326c5ecb2e9
joakinito <144727251+joakinito@users.noreply.github.com>
2024-01-15 20:59:51 +0100

    creado componentes con diseño
    No funciona =(

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3490dc',   
+        secondary: '#ffed4a', 
+      },
+      screens: {
+        'xl': '1280px',  
+      },
+      fontSize: {
+        '3xl': '1.875rem', 
+      },
+    },
+  },
+    },
+  },
+  plugins: [],
+}
+
